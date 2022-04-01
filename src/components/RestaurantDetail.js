133f07a3dfc251fd5b889bc1012b5013377828d5
wael <waeelash155@gmail.com>
2022-03-31 21:21:44 -0400

    final day4

--- src/components/RestaurantDetail.js
+++ src/components/RestaurantDetail.js
@@ -7,7 +7,9 @@ function RestaurantDetail() {
     const [restaurant, setRestaurant] = useState(null);
     const [reviews, setReviews] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
-    
+    const [showForm, setShowForm] = useState(false);
+    const [rating, setRating] = useState(null)
+    const [comment, setComment] = useState("")
     useEffect(() => {
         fetch(`http://localhost:3001/restaurants/${id}`)
           .then((resp) => resp.json())
@@ -17,7 +19,28 @@ function RestaurantDetail() {
             setIsLoaded(true);
           });
     }, [id]);
+        
 
+    function handleSubmit(e){
+        e.preventDefault();
+        const newReview = {
+            restaurantId : parseInt(id),
+            rating : parseInt(rating),
+            comments: comment,
+        }
+        fetch("http://localhost:3001/reviews", {
+        method : "POST",
+        headers : { "Content-Type": "application/json",
+        Accepts: "application/json",},
+        body: JSON.stringify(newReview),
+        })
+        .then((r) => r.json())
+        .then((newReview) => setReviews([...reviews, newReview]))
+
+        e.target.reset();
+
+    }
+    
     if(!isLoaded) return null;
 
     const {image, name, url, address, region, country} = restaurant;
@@ -41,6 +64,14 @@ function RestaurantDetail() {
             <div id="reviews-container">
                 <h1>Reviews</h1>
                 {reviews.length === 0 ? <h3>No Reviews Yet. Add a New Review Below!</h3> : renderReviews}
+                <button id="showReviewForm" onClick={() => setShowForm(!showForm)}><span>Add a Review</span></button>
+            {showForm? 
+            <form id="reviewForm" onSubmit={handleSubmit}>
+                <input onChange={(e) => setRating(e.target.value)} type="number" min={1} max={5} placeholder="Enter a Rating"></input>
+                <input onChange={(e) => setComment(e.target.value)}type="text" placeholder="Enter a Review"></input>
+                <input id="SubmitReview" type="submit" value="Submit Review"></input>
+            </form>
+             : null }
             </div>
         </div>
     );
